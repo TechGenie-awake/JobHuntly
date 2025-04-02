93416c9322567a546c6ff17425e9ccfefc5f0650
TechGenie-awake <techgenie2050@gmail.com>
2025-04-02 13:10:17 +0530

    Added cards to Featured Jobs section

--- src/components/Landing Page/FeaturedJobs.Styled.tsx
+++ src/components/Landing Page/FeaturedJobs.Styled.tsx
@@ -3,7 +3,21 @@ import styled from "styled-components";
 export const StyledFeaturedJobsDiv = styled.div`
     height: 779px;
     width: 100%;
-    margin: 0 124px 72px;
+    padding:0 124px 72px 124px;
+    `;
+
+export const StyledCardsWrapper = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    `;
+
+export const StyledCard = styled.div`
+    height: 283px;
+    width: 274px;
+    border: 1px solid #D6DDEB;
+    margin-top: 30px;
+    padding: 24px;
     `;
 
 export const StyledDivText = styled.h1`
@@ -17,3 +31,76 @@ export const StyledDivText = styled.h1`
         color:#26A4FF;
     }
     `;
+
+export const StyledCardIcon = styled.div`
+    height: 48px;
+    width: 48px;
+    border-radius: 50%;
+    background-color: gray;
+    `;
+
+export const StyledCardRoleText = styled.p`
+@import url('https://fonts.googleapis.com/css2?family=Bai+Jamjuree:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;1,200;1,300;1,400;1,500;1,600;1,700&family=Fahkwang:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;1,200;1,300;1,400;1,500;1,600;1,700&family=Gravitas+One&family=Michroma&family=Poiret+One&family=Spinnaker&family=Unbounded:wght@200..900&display=swap');
+    color:#25324B;
+    font-family: "Fahkwang", sans-serif;
+    font-size: 18px;
+    line-height: 160%;
+    font-weight: 600;
+    `;
+
+export const StyledCardLocationText = styled.p`
+@import url('https://fonts.googleapis.com/css2?family=Epilogue:ital,wght@0,100..900;1,100..900&display=swap');
+    font-family: "Epilogue", sans-serif;
+    font-optical-sizing: auto;
+    font-weight: 400;
+    font-style: normal;
+    font-size: 16px;
+    line-height: 160%;
+    letter-spacing: 1px;
+    color: #515B6F;
+    `;
+
+export const StyledDot = styled.div`
+    width: 4px;
+    height: 4px;
+    border-radius: 50%;
+    background-color: #515B6F;
+    margin-top: 8px;
+    `;
+
+export const StyledCardDescriptionText = styled.p`
+@import url('https://fonts.googleapis.com/css2?family=Epilogue:ital,wght@0,100..900;1,100..900&display=swap');
+    font-family: "Epilogue", sans-serif;
+    font-optical-sizing: auto;
+    font-weight: 400;
+    font-style: normal;
+    font-size: 16px;
+    line-height: 160%;
+    color:#7C8493;
+    `;
+
+export const StyledFullTimeTag = styled.div`
+    width : 91px;
+    height: 31px;
+    border: 1px solid #4640DE;
+    color: #4640DE;
+    padding: 0 auto;
+    text-align: center;
+    `;
+
+export const StyledCardTag = styled.p`
+@import url('https://fonts.googleapis.com/css2?family=Epilogue:ital,wght@0,100..900;1,100..900&display=swap');
+    font-family: "Epilogue", sans-serif;
+    font-optical-sizing: auto;
+    font-weight: 400;
+    font-style: normal;
+    font-size: 16px;
+    line-height: 160%;
+    color:#FFB836;
+    background-color: #EB85331A;
+    padding: 0 8px;
+    text-align: center;
+    margin: 10px 10px 0 0;
+    border-radius: 16px;
+    `;
+
--- src/components/Landing Page/FeaturedJobs.tsx
+++ src/components/Landing Page/FeaturedJobs.tsx
@@ -1,12 +1,50 @@
 'use client';
 
 import React from 'react'
-import { StyledDivText, StyledFeaturedJobsDiv } from './FeaturedJobs.Styled'
+import { StyledDivText, StyledFeaturedJobsDiv, StyledCard, StyledCardsWrapper, StyledCardIcon, StyledCardRoleText, StyledFullTimeTag, StyledCardDescriptionText, StyledCardLocationText, StyledDot, StyledCardTag } from './FeaturedJobs.Styled'
+
+const CardData = [
+    { id: 0, icon: "", role: "Email Marketing", company: "Revolut", location: "Madrid, Spain", description: "Revolut is looking for Email Marketing to help team ma...", tags: ["Marketing", "Design"] },
+    { id: 1, icon: "", role: "Email Marketing", company: "Revolut", location: "Madrid, Spain", description: "Revolut is looking for Email Marketing to help team ma...", tags: ["Marketing", "Design"] },
+    { id: 2, icon: "", role: "Email Marketing", company: "Revolut", location: "Madrid, Spain", description: "Revolut is looking for Email Marketing to help team ma...", tags: ["Marketing", "Design"] },
+    { id: 3, icon: "", role: "Email Marketing", company: "Revolut", location: "Madrid, Spain", description: "Revolut is looking for Email Marketing to help team ma...", tags: ["Marketing", "Design"] },
+    { id: 4, icon: "", role: "Email Marketing", company: "Revolut", location: "Madrid, Spain", description: "Revolut is looking for Email Marketing to help team ma...", tags: ["Marketing", "Design"] },
+    { id: 5, icon: "", role: "Email Marketing", company: "Revolut", location: "Madrid, Spain", description: "Revolut is looking for Email Marketing to help team ma...", tags: ["Marketing", "Design"] },
+    { id: 6, icon: "", role: "Email Marketing", company: "Revolut", location: "Madrid, Spain", description: "Revolut is looking for Email Marketing to help team ma...", tags: ["Marketing", "Design"] },
+    { id: 7, icon: "", role: "Email Marketing", company: "Revolut", location: "Madrid, Spain", description: "Revolut is looking for Email Marketing to help team ma...", tags: ["Marketing", "Design"] }]
+
+function FeaturedJobsCards() {
+    const Cards = CardData.map((card) => (
+        <StyledCard key={card.id}>
+            <div className='flex justify-between mb-8'>
+                <StyledCardIcon>{card.icon}</StyledCardIcon>
+                <StyledFullTimeTag>Full Time</StyledFullTimeTag>
+            </div>
+            <StyledCardRoleText>{card.role}</StyledCardRoleText>
+            <div className='flex items-center justify-between mr-7 mb-6'>
+                <StyledCardLocationText>{card.company}</StyledCardLocationText>
+                <StyledDot />
+                <StyledCardLocationText>{card.location}</StyledCardLocationText>
+            </div>
+            <StyledCardDescriptionText>{card.description}</StyledCardDescriptionText>
+            <div className='flex'>
+                {card.tags.map((tag, index) => (
+                    <StyledCardTag key={index}>{tag}</StyledCardTag>
+                ))}
+            </div>
+        </StyledCard >
+    ))
+
+    return (
+        <StyledCardsWrapper>{Cards}</StyledCardsWrapper>
+    )
+}
 
 const FeaturedJobs = () => {
     return (
         <StyledFeaturedJobsDiv>
             <StyledDivText>Featured <span>jobs</span></StyledDivText>
+            <FeaturedJobsCards />
         </StyledFeaturedJobsDiv>
     )
 }
